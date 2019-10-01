98bd8f9cc6640fef1335d915bfc11abbe7452d9f
biggesee <f.schoettler@olpe.de>
2019-10-01 11:00:11 +0200

    Add files via upload
    Meschede und Bestwig

--- site-meschedebestwig/site.mk
+++ site-meschedebestwig/site.mk
@@ -1,35 +1,42 @@
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	mesh-vpn-tunneldigger \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard
+
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
 	gluon-alfred \
-	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-neighbour-info \
-	gluon-luci-private-wifi \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
+	gluon-mesh-vpn-tunneldigger \
 	gluon-radvd \
+	gluon-respondd \
 	gluon-status-page \
 	gluon-setup-mode \
 	iwinfo \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.5-meschedebestwig-stable$(shell date '+%Y%m%d')
-
-# Allow overriding the release number from the command line
+DEFAULT_GLUON_RELEASE := x2018.2.3-meschedebestwig-$(shell date '+%y%m%d')
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+# Region code required for some images; supported values: us eu
+GLUON_REGION ?= eu
 # Languages to include
 GLUON_LANGS ?= en de
